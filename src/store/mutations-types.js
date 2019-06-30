6b29b3ed7ff5d838e0b6c9ee36c360a50ef91641
victor <834340231@qq.com>
2019-07-01 00:45:10 +0800

    完善商品底部结算列表以及商家评论和信息页面

--- src/store/mutations-types.js
+++ src/store/mutations-types.js
@@ -6,3 +6,7 @@ export const RESET_USER_INFO = 'reset_user_info' // 重置用户信息
 export const RECEIVE_GOODS = 'receive_goods' // 接 收 商 品 数 组
 export const RECEIVE_RATINGS = 'receive_ratings' // 接 收 商 家 评 价 数 组
 export const RECEIVE_INFO = 'receive_info' // 接 收 商 家
+export const INCREMENT_FOOD_COUNT = 'increment_food_count' // 增加food中的count
+export const DECREMENT_FOOD_COUNT = 'decrement_food_count' // 减少food中的count
+export const CLEAR_CART = 'clear_cart' // 清空购物车
+export const RECEIVE_SEARCH_SHOPS = 'receive_search_shops' // 接 收 搜索商 家
--- src/store/state.js
+++ src/store/state.js
@@ -7,5 +7,7 @@ export default{
   userInfo: {}, // 用户信息
   goods: [], // 商品列表
   ratings: [], // 商家评价信息
-  info: {}// 商家信息
+  info: {}, // 商家信息
+  cartFoods: [], // 购物车中食物的列表
+  searchShops: []// 搜索得到的商家列表
 }
